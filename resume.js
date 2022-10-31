0e3e984d12bf5734c09b085b5073dacb40254506
bharath kumar <108915815+bestieeeeee@users.noreply.github.com>
2022-10-31 15:15:59 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,10 @@
+var resume=[{"Name":"Bharathkumar.e",
+             "Qualification":"B.E",
+             "Email":"bharathkumar.ekbtgmail.com",
+            "Experience":"Fresher",
+        "Address":"154/164,prumampalyam, ayyampalyam(po)kavinthapadi Erode",
+    "Domain":"Full Stack Development",}];
+ //console.log(resume);
+resume.forEach(function() {
+    console.log(resume);
+});
